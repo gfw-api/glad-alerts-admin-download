cbb8e6bfdd8f3c51f3a4d5751b38f2bc0c1f77a3
mappingvermont <charlie.hofmann@gmail.com>
2018-08-22 12:53:51 -0400

    start to implement s3 download

--- app/src/routes/api/v1/service.router.js
+++ app/src/routes/api/v1/service.router.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router');
+const AWS = require('aws-sdk');
+const PassThrough = require('stream').PassThrough;
 
 
 const router = new Router({
@@ -8,9 +10,32 @@ const router = new Router({
 class Service {
 
     static sayHi(ctx) {
-        ctx.body = {
-            greeting: 'hi'
-        };
+
+      // https://stackoverflow.com/a/27993593/
+      var s3 = new AWS.S3()
+      var params = {Bucket: 'gfw2-data',
+                    Key: 'alerts-tsv/temp/glad-by-state/BRA/BRA_14.csv'};
+
+      // var resp = new Promise((resolve, reject) => {
+      //
+      //   s3.getObject(params)
+      //     .createReadStream()
+      //     .on('end', () => {
+      //       return resolve(); })
+      //     .on('error', (error) => {
+      //       console.log(error)
+      //       return reject(error); })
+      //     .pipe(PassThrough())});
+
+        //ctx.body = ctx.req.pipe(resp)
+
+        // kind of works, but waits for a long time before download
+        // in browser starts
+        var resp = s3.getObject(params)
+          .createReadStream()
+          .pipe(PassThrough());
+
+        ctx.body = resp;
     }
 
 }
